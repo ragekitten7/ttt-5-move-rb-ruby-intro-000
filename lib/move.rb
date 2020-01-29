fc36e88e9925b3509f9b73f82173377bdb23e1f8
Sam Bryant <bryantsj@gmail.com>
2020-01-29 22:39:09 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, player="X")
-  array[input_to_index] = player
+def move(board, index, player = "X")
+  array[index] = player
 end
